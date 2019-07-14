4b45f0c2928ddc892bd01f18232ae2996997f297
KarolChilimoniuk <webdevkarolchilimoniuk@gmail.com>
2019-07-14 09:57:01 +0200

    script & style for mobile navigation

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,9 @@
-"use strict";
+const hamburgerButton = document.querySelector('.menu__switcher--js');
+const hamburgerIcon = document.querySelector('.fa-bars');
+const menu = document.querySelector('.navigation');
 
-console.log(`Hello world!`);
+hamburgerButton.addEventListener('click', function () {
+   menu.classList.toggle('navigation--visible');
+   hamburgerIcon.classList.toggle('fa-bars');
+   hamburgerIcon.classList.toggle('fa-times');
+})
